1ac8fcbeec692d94b8cec52556c32f44d0df9261
Duhan Meric <duhanmeric@gmail.com>
2021-04-16 16:10:37 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import { useRef, useEffect } from "react";
 
 function App() {
+  var cursorRef = useRef();
+
+  useEffect(() => {
+    document.addEventListener("mousemove", function (e) {
+      var x = e.clientX;
+      var y = e.clientY;
+      cursorRef.current.style.left = x + "px";
+      cursorRef.current.style.top = y + "px";
+    });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="cursor" ref={cursorRef}>
+        <div id="innerCursor"></div>
+      </div>
+      <Header />
+      <Hero />
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+export default function Header() {
+  return (
+    <header>
+      <div className="author">JOHN DOE</div>
+      <nav>
+        <ul className="list-unstyled mb-0">
+          <li>Work</li>
+          <li>About</li>
+          <li>Playlist</li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
