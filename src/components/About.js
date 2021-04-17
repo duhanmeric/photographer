0a4f73ed34e67afc8c9440501783ead265ccbacd
Duhan Meric <duhanmeric@gmail.com>
2021-04-17 21:41:10 +0300

    about page

--- src/components/About.js
+++ src/components/About.js
@@ -3,10 +3,6 @@ import about from "../assets/img/about.jpg";
 export default function About() {
   return (
     <section className="about-section text-center text-white">
-      <div className="top-left-frame">
-        <div className="horizontal-line"></div>
-        <div className="vertical-line"></div>
-      </div>
       <h1 className="text-center">
         ABOUT <span style={{ fontWeight: "bold" }}>MYSELF</span>
       </h1>
@@ -24,10 +20,6 @@ export default function About() {
         veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
         commodo consequat.
       </p>
-      <div className="bottom-right-frame">
-        <div className="vertical-line"></div>
-        <div className="horizontal-line"></div>
-      </div>
     </section>
   );
 }
